8caba5a0fd2b7dd467e7d78664e7dc879175bc80
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-12-23 21:22:07 +0530

    just11

--- 04-loops/main.tf
+++ 04-loops/main.tf
@@ -11,6 +11,9 @@ module "ec2" {
   sg_id = module.sg.security_group_id
 }
 
+output "all_from_ec2" {
+  value = module.ec2
+}
 output "public_ip" {
   value = {
     for k, v in module.ec2: k=> v["ec2"].public_ip
