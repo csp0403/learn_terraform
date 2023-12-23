618945d4490d0a5668875c48d826bc4de3e032f8
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-12-23 16:02:42 +0530

    just11

--- 02-resources/main.tf
+++ 02-resources/main.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "first" {
   ami = data.aws_ami.example.id
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  availability_zone = "us-east-1a"
 
   tags = {
   Name ="Automation_ec2"
